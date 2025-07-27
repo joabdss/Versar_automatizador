07b8c596313973d40d752ec01a077c34bf91a51d
joabdss <joabe_silva_sena@hotmail.com>
2025-07-27 19:04:03 -0300

    3 Clicar "+ Criar Módulo"
    Clicar em criar módulo
    
    Resultado:
    
    Redirecionamento para a página de criação de módulos
    
    Próxima Etapa:
    
    4. Preencher o forms

--- T_versar.py
+++ T_versar.py
@@ -1,12 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 
+
+
+
 driver = webdriver.Chrome(service=service)
+
+wait = WebDriverWait(driver, 10)
+
+#Abrir Navegador e Site
 driver.get("https://playful-torrone-162c28.netlify.app")
 sleep(2)
 
+#Clicar em Login
 login_button = driver.find_element(By.XPATH, '//span[tect()="Login]')
 login_button.click()
 
@@ -17,6 +27,13 @@ driver.find_element(By.NAME, "password").send_keys("versar123")
 
 #Efetua o login e entra na página
 driver.find_element(By.TAG_NAME, "form").submit()
+
+#Clicar em '+ Criar Módulo'
+criar_modulo_btn = wait.until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.MuiButton-containedPrimary'))
+)
+criar_modulo_btn.click()
+
 
 sleep(10)
 #driver.quit()
